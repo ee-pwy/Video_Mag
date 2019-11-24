e1a2a27f5282bc91e74db570ce57d33a6a8a8d41
penww16@gmail.com <penww16@gmail.com>
2019-11-24 15:48:19 -0800

    version6

--- magnet.py
+++ magnet.py
@@ -76,7 +76,7 @@ class Net(nn.Module):
         shape = self.pad_1(x)
         shape = F.relu(self.bn_shape_1(self.conv_shape_1(shape)))
         shape = self.pad_1(shape)
-        shape = F.relu(self.bn_shape_1(self.conv_shape_2(shape)))
+        shape = F.relu(self.bn_shape_2(self.conv_shape_2(shape)))
         for block in self.res_blk_shape:
             shape = block(shape)
 
